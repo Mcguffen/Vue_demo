d81cc7cb985997abca0289789d5e65d65f4269a3
Poetic Justice <mcguffen@hanshanyang.xyz>
2019-04-30 17:08:01 +0800

    Vue项目的两种构建方式，cdn和vue create 项目名

---
+++ hello-world/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+        <ol>
+            <todo-item v-for = "item in groceryList" v-bind:todo="item" v-bind:key="item.id">
+
+            </todo-item>
+        </ol>
+  </div>
+</template>
+
+<script>
+import TodoItem from './components/TodoItem.vue'
+
+export default {
+  name: 'app',
+  data(){
+    return{
+      groceryList: [
+      {    
+          id: 0,
+          text: '🧀️'
+      },{
+          id: 1,
+          text: '👃'
+      },{
+          id: 2,
+          text: '🚚'
+      }]
+    }
+  },
+  components: {
+    TodoItem
+  }
+}
+</script>
---
+++ hello-world/src/components/TodoItem.vue
@@ -0,0 +1,16 @@
+<template>
+  <li class="text">{{ todo.text}}</li>
+</template>
+
+<script>
+export default {
+  props: ['todo'],
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .text{
+    color: rgb(21, 30, 37);
+  }
+</style>
