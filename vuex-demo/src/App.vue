8f1cddd27a0a76455c59273b9b469aa85bccee62
Poetic Justice <mcguffen@hanshanyang.xyz>
2019-04-30 18:59:08 +0800

    Vuex-demo

---
+++ vuex-demo/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    {{count}}
+    
+    {{$store.getters.doubleCount}}
+    <button @click="$store.commit('increment', 2)">count++</button>
+    <button @click="$store.dispatch('increment', 2)">count++</button>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+  computed: {
+    count(){
+      return this.$store.state.count
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
